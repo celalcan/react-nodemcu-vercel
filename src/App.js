75339094459087463b3bc3377e64b6b5448e7bac
celalcan <celalcanh.h.oglu@gmail.com>
2024-08-17 17:29:02 +0300

    Modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const turnOn = () => {
+    fetch("http://192.168.2.128/on")
+      .then(response => response.text())
+      .then(data => console.log(data));
+  };
+
+  const turnOff = () => {
+    fetch("http://192.168.2.128/off")
+      .then(response => response.text())
+      .then(data => console.log(data));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LED Control</h1>
+      <button onClick={turnOn}>Turn On</button>
+      <button onClick={turnOff}>Turn Off</button>
     </div>
   );
 }
